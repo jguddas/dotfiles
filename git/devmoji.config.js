0ed0f0a11f58c624ad4b1138b9eea2e282891a59
Jakob Guddas <github@jguddas.de>
2023-05-30 18:10:05 +0200

    feat(git): ✨ added devmoji config

---
+++ git/devmoji.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  types: ["hotfix", "revert", "merge"],
+  devmoji: [
+    { code: "hotfix", gitmoji: "ambulance" },
+    { code: "revert", gitmoji: "rewind" },
+    { code: "merge", gitmoji: "see_no_evil" },
+    { code: "a11y", gitmoji: "wheelchair" },
+    { code: "ci", emoji: "robot" },
+  ],
+};
